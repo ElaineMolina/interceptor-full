394bd7b8ff3bd2da712bd877b36fd4c8730b58f7
Elaine.Molina <elaine.molina@capgemini.com>
2023-06-09 17:06:30 -0300

    Implementando pesquisa de serviços prestados

--- clientes-app-template/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ clientes-app-template/src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -10,16 +12,24 @@ export class ServicoPrestadoListaComponent implements OnInit {
   nome: string;
   mes: number;
   meses: number[];
+  lista: ServicoPrestadoBusca[];
+  message: string;
 
-  constructor() {
-    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12];
-   }
+  constructor(private service: ServicoPrestadoService) {
+    this.meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  }
 
   ngOnInit(): void {
   }
 
-  consultar(){
-    console.log(this.nome, this.mes);
-  }
-
+  consultar() {
+    this.service.buscar(this.nome, this.mes).subscribe(response => {
+      this.lista = response;
+      if(this.lista.length <= 0){
+        this.message = "Nenhum Resgistro encontrado."
+      }else{
+        this.message = null;
+      };
+  });
+}
 }
