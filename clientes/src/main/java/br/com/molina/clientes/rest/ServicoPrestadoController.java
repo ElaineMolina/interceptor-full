4fe17328ed3c7795c20fd1ea0c9c5bcc7dca1c12
Elaine.Molina <elaine.molina@capgemini.com>
2023-06-09 16:04:23 -0300

    Mensagem de feedback no formnulário de Serviços Prestados

--- clientes/src/main/java/br/com/molina/clientes/rest/ServicoPrestadoController.java
+++ clientes/src/main/java/br/com/molina/clientes/rest/ServicoPrestadoController.java
@@ -27,7 +27,7 @@ public class ServicoPrestadoController  {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ServicoPrestado salvar( @RequestBody ServicoPrestadoDTO dto ){
+    public ServicoPrestado salvar( @RequestBody @Valid ServicoPrestadoDTO dto ){
         LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         Integer idCliente = dto.getIdCliente();
 
--- clientes/src/main/java/br/com/molina/clientes/rest/dto/ServicoPrestadoDTO.java
+++ clientes/src/main/java/br/com/molina/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -1,13 +1,23 @@
 package br.com.molina.clientes.rest.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class ServicoPrestadoDTO {
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 }
